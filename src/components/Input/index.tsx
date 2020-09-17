fea5cb9f38ed467c66a20b0fdf13c859864ad466
Allan Melo <allmelo@users.noreply.github.com>
2020-09-17 01:39:32 -0300

    Displaying input errors

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -48,6 +48,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...otherProps }) => {
         ref={inputRef}
         {...otherProps}
       />
+      {error}
     </StyledInput>
   );
 };
